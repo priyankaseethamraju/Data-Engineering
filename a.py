a9ed2f98632e7c31007f7e77834d8888767eaff2
priyankaseethamraju <seet@pdx.edu>
2021-03-06 07:58:38 -0800

    data gatherer

--- a.py
+++ a.py
@@ -2,11 +2,12 @@ import urllib.request
 import json
 
 with urllib.request.urlopen('http://rbi.ddns.net/getBreadCrumbData') as response:
-    with open('venv/b.json', 'wb') as file:
+    with open('b.json', 'wb') as file:
         file.write(response.read())
 
-reader = open('venv/b.json')
+reader = open('b.json')
 
 y=json.load(reader)
 #y = json.loads(b.json)
 print(y)
+
